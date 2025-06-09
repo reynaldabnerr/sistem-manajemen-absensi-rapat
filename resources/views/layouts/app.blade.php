8c5ff2bf56d8abf096c6bfdf7994552ff1a1233e
Muhammad Rifqi <muhrifqisirajuddin@gmail.com>
2025-06-09 19:36:37 +0800

    update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Absensi Rapat</title>
+    <link rel="icon" href="{{ asset('logo/unhas.png') }}" type="image/png">
     <!-- Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
